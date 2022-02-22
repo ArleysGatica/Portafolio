ed5fafa5c29499a0a3be6c179f38f03173539083
ArleysGatica <gatica.arleys16@gmail.com>
2022-02-22 17:28:21 -0600

    termanando footer,info-perfil

--- src/Frontend/src/App/Components/Footer/Footer.jsx
+++ src/Frontend/src/App/Components/Footer/Footer.jsx
@@ -1,20 +1,29 @@
 import React from "react";
+import "./Footer.css";
 
 export const Footer = () => {
   const Footerimages = require.context('../../Images', true)
   return (
     <footer className="footer">
       <div className="container">
-        <div className="Container-footer-red">
+        <div className="Container-footer-whatsapp">
+          <h4>¿Alguna Duda? Contactame...</h4>
           <a
             rel="noreferrer"
-            href="https://github.com/ArleysGatica"
+            href="https://api.whatsapp.com/send?phone=+505 8634-9918"
             target="_blank"
           >
-            <img src={Footerimages('./github.png')} alt="" />
+            <button>(+505) 8634-9918</button>
           </a>
         </div>
         <div className="Container-footer-red">
+          <a
+            rel="noreferrer"
+            href="https://www.facebook.com/arleys.gatica.34"
+            target="_blank"
+          >
+            <img src={Footerimages('./facebook.png')} alt="" />
+          </a>
           <a
             rel="noreferrer"
             href="https://www.linkedin.com/in/arleys-gatica16/"
@@ -22,29 +31,28 @@ export const Footer = () => {
           >
             <img src={Footerimages('./linkedin.png')} alt="" />
           </a>
-        </div>
-        <div className="Container-footer-red">
           <a
             rel="noreferrer"
-            href="https://api.whatsapp.com/send?phone=+505 8634-9918"
+            href="https://github.com/ArleysGatica"
             target="_blank"
           >
-            <img src={Footerimages('./whatsapp.png')} alt="" />
-            <button>+505 8634-9918</button>
+            <img src={Footerimages('./github.png')} alt="Github" />
           </a>
+          <a
+            rel="noreferrer"
+            href="https://www.instagram.com/arleyssss_16/"
+            target="_blank"
+          >
+            <img src={Footerimages('./instagram.png')} alt="" />
+          </a>
+
+
         </div>
-        <div className="Container-pie">
-          <p>
-            &copy; Copyright 2020. Todos los derechos reservados.
-            <a href="https://www.linkedin.com/in/jose-javier-gonzalez-gonzalez-a8a8b5b1/" target="_blank" rel="noopener noreferrer">
-              <p className="fab fa-linkedin">mamamamamam</p>
-            </a>
-            <a href="
-              " target="_blank" rel="noopener noreferrer">
-              <p className="fab fa-github">putas</p>
-            </a>
-          </p>
-        </div>
+      </div>
+      <div className="Container-pie">
+        <p>
+          &copy; Copyright 2022. Designed by El_Nicho.
+        </p>
       </div>
     </footer>
   );
