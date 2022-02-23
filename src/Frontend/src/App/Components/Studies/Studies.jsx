867caf8e66dba8f685272e479aa7a7476caecf8a
ArleysGatica <gatica.arleys16@gmail.com>
2022-02-23 00:20:49 -0600

    Añadiendo studies-page

--- src/Frontend/src/App/Components/Studies/Studies.jsx
+++ src/Frontend/src/App/Components/Studies/Studies.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import './Studies.css';
 
 export const Studies = () => {
-  const Studiesimages = require.context('../../Images', true)
+  const Studiesimages = require.context('../../Images/Certi', true)
   return (
     <>
       <div className="Container-title-Studies">
-        <h1>tapa</h1>
         <h1>Estudios</h1>
-        <h1>s</h1>
       </div>
       <div className="Studies-Container">
-        <img src={Studiesimages('./css-3.png')} alt="" />
+        <img src={Studiesimages('./Udemy3.png')} alt="" />
+        <img src={Studiesimages('./react-pr.png')} alt="" />
+        <img src={Studiesimages('./Asincrrono Js.png')} alt="" />
+        <img src={Studiesimages('./udemy2.png')} alt="" />
+        <img src={Studiesimages('./basica.png')} alt="" />
+        <img src={Studiesimages('./certif.png')} alt="" />
+        <img src={Studiesimages('./front.png')} alt="" />
+        <img src={Studiesimages('./jdom.png')} alt="" />
+        <img src={Studiesimages('./React-redux.png')} alt="" />
+        <img src={Studiesimages('./Udemy.png')} alt="" />
+        <img src={Studiesimages('./webpack.png')} alt="" />
       </div>
     </>
   );
