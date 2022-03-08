60c6e8f1fd055ca3297f16bb0ce0ab43b29dae17
ArleysGatica <gatica.arleys16@gmail.com>
2022-03-07 18:50:42 -0600

    navBar

--- Frontend/src/App/Components/Header/Header.jsx
+++ Frontend/src/App/Components/Header/Header.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Header.css'
+import { VscThreeBars } from 'react-icons/vsc'
 
 export const Header = () => {
+
+  const [show, setShow] = useState(false)
+  const hadleClick = () => {
+    setShow(!show)
+  }
+
   return (
     <>
       <header className="header">
         <div className="Container-header">
-          <nav className="nav">
-            <a href="./" >Home</a>
-            <NavLink to="/Studies">Estudios</NavLink>
-            <NavLink to="/Projects">Proyectos</NavLink>
-            <a href="#Sobremi" > Sobre mi</a>
-          </nav>
+          <button onClick={hadleClick}>
+            <VscThreeBars />
+          </button>
+          <div
+            className={
+              show
+                ? `HomeContainer__NavBar__Rigth show`
+                : `HomeContainer__NavBar__Rigth`
+            }
+          >
+            <nav className="nav">
+              <a href="./" >Home</a>
+              <NavLink to="/Studies">Estudios</NavLink>
+              <NavLink to="/Projects">Proyectos</NavLink>
+              <a href="#Sobremi" > Sobre mi</a>
+            </nav>
+          </div>
         </div>
       </header>
     </>
